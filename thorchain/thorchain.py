2c03d0c5f85527f41447be0530505bcb012e48f8
Son of Odin <sonofodin-thorchain@protonmail.com>
2022-03-01 10:12:22 -0500

    fix source/target detection

--- thorchain/thorchain.py
+++ thorchain/thorchain.py
@@ -1159,7 +1159,7 @@ class ThorchainState:
 
         # refund if we're trying to swap with the coin we given ie
         # swapping bnb with bnb
-        if source == target and source.is_synth == target.is_synth:
+        if source == target:
             reason = "swap Source and Target cannot be the same.: unknown request"
             return self.refund(tx, 105, reason)
 
